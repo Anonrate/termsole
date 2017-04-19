24f9629619e03ed1f9d9fda5c38dabad5be20bee
Anonrate <Anonrate@Outlook.com>
2017-04-19 15:09:37 -0400

    Added src and header.

---
+++ inc/termsole.h
@@ -0,0 +1,31 @@
+/**
+ *  @file     termsole.h
+ *  @brief    termsole.h
+ *  @version  v.1
+ *  @date     04/19/2017 15:07:52
+ *  @author   Anonrate
+ *  @copyright
+ *    \parblock
+ *      GNU General Public License
+ *
+ *      Copyright (C) 2017 Anonrate
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by
+ *        the Free Software Foundation, either version 3 of the License, or
+ *        (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    \endparblock
+ */
+
+#ifndef TERMSOLE_H
+#define TERMSOLE_H
+
+#endif  /* TERMSOLE_H */
