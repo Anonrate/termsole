a838d39a769a56df9f95a1d742ee751976f8778a
Anonrate <Anonrate@Outlook.com>
2017-04-19 21:41:05 -0400

    Added linuxterm.h

---
+++ inc/linux/linuxterm.h
@@ -0,0 +1,88 @@
+/**
+ *  @file     linuxterm.h
+ *  @brief    linuxterm.h
+ *  @version  v.1
+ *  @date     04/19/2017 19:53:12
+ *  @author   Anonrate
+ *  @copyright
+ *    \parblock
+ *      GNU General Public License
+ *
+ *      Copyright (C) 2017 Anonrate
+ *
+ *      This program is free software: you can redistribute it and/or modify
+ *        it under the terms of the GNU General Public License as published by
+ *        the Free Software Foundation, either version 3 of the License, or
+ *        (at your option) any later version.
+ *
+ *      This program is distributed in the hope that it will be useful,
+ *        but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *        GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *        along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *    \endparblock
+ */
+
+#ifndef LINUXTERM_H
+#define LINUXTERM_H
+
+#include  "cntrlseq/formats.h"
+#include  "cntrlseq/fgcols.h"
+#include  "cntrlseq/bgcols.h"
+
+static enum fgcol   lstfg   = FG_DEF;
+static enum bgcol   lstbg   = BG_DEF;
+
+int
+isboldset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmtbold;
+}
+
+int
+isdimset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmtdim;
+}
+
+int
+isunderlinedset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmtunderlined;
+}
+
+int
+isblinkset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmtblink;
+}
+
+int
+isreverseset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmtreverse;
+}
+
+int
+ishiddenset(void)
+{
+    INFUNC_MSGL(DEBUG);
+    return lstfmts.fmthidden;
+}
+
+
+
+
+
+
+
+
+
+#endif  /* LINUXTERM_H */
